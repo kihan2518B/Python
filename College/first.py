74974434bb36ec27bcd13c0dc2e59c3079cc35d1
kihan2518B <kp648027@gmail.com>
2024-03-01 00:41:23 +0530

    writen first program of ce syllabus

---
+++ College/first.py
@@ -0,0 +1,10 @@
+# Getting input from user
+
+userName = input("Enter Your name:")
+# Age
+userAge = int(input("Enter Your Age:"))
+
+# Logic
+yearToBe100 = (100-userAge)+2024
+
+print("You will become 100 years old in year",yearToBe100)
