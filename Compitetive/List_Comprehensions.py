58ac73acc5a5cd8eaa8dfb4dcede87af33c9fa71
kihan2518B <kp648027@gmail.com>
2024-03-15 14:10:34 +0530

    uncommented the code

--- Compitetive/List_Comprehensions.py
+++ Compitetive/List_Comprehensions.py
@@ -3,13 +3,13 @@
 # Syntax
 # NewList = [expression for item in iterable if condition == true]
 
-# x = int(input())
-# y = int(input())
-# z = int(input())
-# n = int(input())
-#
-# newList = [[i,j,k] for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1)  if(i+j+k != n)]
-# print(newList)
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
 
-SecondList = [i if i%2 == 1 else 0 for i in range(0,5) ]
-print(SecondList)
+newList = [[i,j,k] for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1) if(i+j+k != n)]
+print(newList)
+
+# SecondList = [i if i%2 == 1 else 0 for i in range(0,5) ]
+# print(SecondList)
