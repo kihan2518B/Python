3b70e25961f8dcad1fdf403335c9b134b6c972c0
kihan2518B <kp648027@gmail.com>
2024-06-08 23:01:14 +0530

    1 problems sovled 8-06-24

---
+++ Compitetive/Math/PolarCoordinates.py
@@ -0,0 +1,8 @@
+# abs method returns absolute value or root of X^2+Y^2
+# phase method returns the phase angle between z and x axis
+import cmath
+
+z = complex(input())
+
+print(abs(z))
+print(cmath.phase(z))
